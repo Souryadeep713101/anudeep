157dc38f7af2d2de37e08e7158f95e43cfa5b53f
hackerank <me@hackerank.com>
2024-01-08 22:07:29 +0530

    Programme View and Sign Up Page

--- src/Components/ProgrammeView.jsx
+++ src/Components/ProgrammeView.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SignUp from './SignUp'
 
 export default function ProgrammeView() {
-  return (
-    <div>ProgrammeView</div>
+  const [isProgramSelected,setIsProgramSelected] = useState(false);
+  return isProgramSelected?<SignUp/>:(
+    <div className='max-w-md mx-auto border border-blue-700 rounded-lg shadow-lg m-2 p-4 justify-center'>ProgrammeView
+
+      <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+      <label for="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a Program</label>
+      <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option selected>Choose Your Program</option>
+        <option value="US">Java</option>
+        <option value="CA">Python</option>
+        <option value="FR">C++</option>
+        <option value="DE">C#</option>
+      </select>
+
+      <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Program Details</label>
+      <textarea readOnly id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""></textarea>
+
+      <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mentor Duties</label>
+      <textarea readOnly id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""></textarea>
+      </div>
+      <div className='text-center m-2 p-4'>
+      <button onClick={()=>setIsProgramSelected(true)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Continue</button>
+
+      </div>
+    </div>
   )
 }
---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,63 @@
+import React, { useRef } from "react";
+
+export default function SignUp(){
+    const form = useRef(),full_name = useRef(null),email=useRef(null),phone=useRef(null),designation=useRef(null),company_name=useRef(null),linkedin_id=useRef(null),adhaar_id = useRef(null);
+    const data = [];
+    const handleFormSubmission = ()=>{
+        if(full_name&&email&&phone&&designation&&company_name&&linkedin_id&&adhaar_id){
+            const curr_data = {
+                name:full_name.current.value,
+                email:email.current.value,
+                phone:phone.current.value,
+                designation:designation.current.value,
+                company_name:company_name.current.value,
+                linkedin_id:linkedin_id.current.value,
+                adhaar_id:adhaar_id.current.value,
+            }
+            data.push(curr_data);
+            form.current.reset();
+            console.log(data);
+        }
+    }
+    return(
+        <>
+            <form ref={form} onSubmit={(e)=>e.preventDefault()} className="max-w-md mx-auto border border-blue-700 m-2 p-4 bg-gray-100 rounded-lg shadow-lg">
+                <div className="relative z-0 w-full mb-5 group">
+                    <input ref={full_name} type="text" name="floating_full_name" id="floating_full_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full name</label>
+                </div>
+                {/* <div className="relative z-0 w-full mb-5 group">
+                    <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+                </div> */}
+                <div className="relative z-0 w-full mb-5 group">
+                    <input ref={email} type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input ref={phone} type="tel" pattern="^[6-9]\d{9}$" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (9876543210)</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input ref={designation} type="text" name="floating_designation" id="floating_designation" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Designation (Ex. System Engineer)</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input ref={company_name} type="text" name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. TCS)</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input ref={linkedin_id} pattern="^https:\\/\\/[a-z]{2,3}\\.linkedin\\.com\\/.*$" type="text" name="floating_linkedin_id" id="floating_linkedin_id" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Linkedin ID</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input ref={adhaar_id} type="text" pattern="^[0-9]{4}$" name="floating_id" id="floating_id" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ID Validation(Last 4 digits)</label>
+                </div>
+                
+            <button type="submit" className="ml-[38%] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleFormSubmission}>Submit</button>
+            </form>
+
+        </>
+    )
+}
