232819f10b2093044670ad1b30bbd577cfbd788c
Souryadeep713101 <dassouryadeep10@gmail.com>
2024-01-20 11:34:28 +0530

    Sixth version

--- src/Components/MentorCommitment.jsx
+++ src/Components/MentorCommitment.jsx
@@ -2,6 +2,7 @@ import React , {useState} from 'react'
 import { Datepicker } from 'flowbite-react';
 import CommitmentCalendar from './CommitmentCalendar';
 import { toast } from 'react-toastify';
+import axios from 'axios';
 
 export default function MentorCommitment() {
 
@@ -16,7 +17,7 @@ export default function MentorCommitment() {
     } else {
       setSelectedDays([...selectedDays, day]);
     }
-    setFormData((prevFormData)=>{ return { ...prevFormData, ["selected-days"] : [...selectedDays ,day] }})
+    setFormData((prevFormData)=>{ return { ...prevFormData, selectedDays : [...selectedDays ,day] }})
   };
 
 
@@ -24,77 +25,86 @@ export default function MentorCommitment() {
    setFormData((prevFormData)=>{
     return {...prevFormData , [e.target.name]  : e.target.value}
    })
-
+   if(e.target.name == "timeStart") document.getElementById("time-input-end").setAttribute("min" , e.target.value)  
 
  } 
-const onSubmit = (e)=>{
+const onSubmit = async (e)=>{
 e.preventDefault();
+try{
 
+
+ console.log(formData)
+
+ const response = await axios.post("https://ae731edf-571c-430a-b094-9cce57a25653.mock.pstmn.io/mentor/commit"  , {useId : "souryadeep123" , ...formData } )
+ console.log(response.status)
 toast.success('🦄 Commited Successfully');
-console.log(formData)
+
 e.target.reset()
 setSelectedDays([])
 setFormData({})
+}
+catch(e) {
+
+}
+
 
 }
 
 
   return (
-<div className='flex items-center justify-between w-full h-full flex-col justify-around'>  
-<form className='flex flex-col align-center justify-between w-1/2  p-10 border bg-white border border-gray-200 rounded-lg shadow-lg' onSubmit={onSubmit}>
+<div className='flex items-center justify-between w-full h-full   flex-col justify-around'>  
+<form className='flex flex-col align-center justify-between  min-w-fit w-1/2 p-10  bg-white border border-gray-200 rounded-lg shadow-lg' onSubmit={onSubmit}>
 
 
 <div className='input-container'>
-<label htmlFor="select-programme" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Programme</label>
-<select  onChange = {onChange}name="select-programme" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-<option value="NULL">Select A Programme</option>
+<label htmlFor="selectedProgramme" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Programme</label>
+<select   name="selectedProgramme" onChange = {onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+<option value="" disabled selected hidden>Select Programme</option>
   <option value="Java">Java</option>
   <option value="C++">C++</option>
   <option value="Python">Python</option>
   <option value="React">React</option>
 </select>
 </div>
-<div className='input-container'><label htmlFor="programme-tenure" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Programme Tenure</label>
-<select   onChange = {onChange} required name="programme-tenure" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-<option value="NULL">Select Tenure</option>
-  <option value="1year">1year</option>
-  <option value="2year">2year</option>
-  <option value="3year">3year</option>
-  <option value="4year">4year</option>
-</select>
+<div className='input-container'><label htmlFor="programmeTenure" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Programme Tenure(in months)</label>
+<input defaultValue="1" min="1" onChange = {onChange}  type="number" required name="programmeTenure" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+
+
 </div>
 <div className='input-container'>
-<label htmlFor="programme-tenure" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>  
-<Datepicker onSelectedDateChanged={(date)=>{
+<label  className="block mb-2 text-sm font-medium text-gray-900">Start Date</label>  
+<Datepicker minDate={new Date()} 
+onSelectedDateChanged={(date)=>{
   setFormData((prevFormData)=>{
-    return {...prevFormData , ["start-date"] : date }
+    return {...prevFormData , startDate : date }
   })
 }}  required/>
 </div>
-<div className='flex justify-between input-container'>
-  <div >
-  <label htmlFor="time-input-start" className="block text-sm font-medium text-gray-700 w-full">Start Time</label>
-  <input
-  onChange={onChange}
-  required
-    type="time"
-    id="time-input"
-    name="time-input-start"
-    className="mt-1 p-2.5 border rounded-md w-full focus:outline-none focus:ring focus:border-blue-300"
-  />
+<div className='flex flex-row justify-between input-container'>
+  <div className="mb-4 md:mb-0">
+    <label htmlFor="timeStart" className="block text-sm font-medium text-gray-700">Start Time</label>
+    <input
+      onChange={onChange}
+      required
+      type="time"
+      id="time-input"
+      name="timeStart"
+      className="mt-1 p-2.5 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+    />
+  </div>
+  <div className="md:ml-4">
+    <label htmlFor="time-input" className="block text-sm font-medium text-gray-700">End Time</label>
+    <input
+      onChange={onChange}
+      required
+      type="time"
+      id="time-input-end"
+      name="timeEnd"
+      className="mt-1 p-2.5 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+    />
+  </div>
 </div>
-<div >
-<label htmlFor="time-input" className="block text-sm font-medium text-gray-700">End Time</label>
-  <input
-  onChange={onChange}
-  required
-    type="time"
-    id="time-input-end"
-    name="time-input-end"
-    className="mt-1 p-2.5 border rounded-md w-full focus:outline-none focus:ring focus:border-blue-300"
-  />
- </div> 
-</div>
+
 
 <div>
 
@@ -107,7 +117,7 @@ setFormData({})
            type='button'
             key={day}
             onClick={() => toggleDay(day)}
-            className={`w-12 h-12 flex items-center justify-center rounded-full focus:outline-none ${
+            className={`lg:w-12 lg:h-12 sm:text-sm flex items-center justify-center rounded-full focus:outline-none ${
               selectedDays.includes(day) ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
             }`}
           >
--- src/Components/ProgrammeView.jsx
+++ src/Components/ProgrammeView.jsx
@@ -3,13 +3,16 @@ import SignUp from './SignUp'
 
 export default function ProgrammeView() {
   const [isProgramSelected,setIsProgramSelected] = useState(false);
+  const onChange = ()=>{
+    setIsProgramSelected(true)
+  }
   return isProgramSelected?<SignUp/>:(
     <div className='max-w-md mx-auto border border-blue-700 rounded-lg shadow-lg m-2 p-4 justify-center'>ProgrammeView
 
       <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
       <label for="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a Program</label>
-      <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <select onChange={onChange} id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option selected>Choose Your Program</option>
         <option value="US">Java</option>
         <option value="CA">Python</option>
